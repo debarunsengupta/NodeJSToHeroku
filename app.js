8f3bf510143364dc5bd9286eec0b9c5e5ab98a13
Debarun <debarunsengupta2512@gmail.com>
2018-09-20 16:35:27 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const port=process.env.PORT || 3000
+var expres=require('express');
+var app=expres();
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
+});
+app.get('/',function (req,res){
+    //res.send('Swarup Bam');
+   res.setHeader('Content-Type', 'application/json');
+   
+                    res.send(JSON.stringify({
+                        "speech" : 'connection achieved',
+                        "displayText" : 'connection achieved'
+                    }));
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
