34e37df1064280e8c7bdb8fbfc3551f2a3abe9cb
debarunsengupta <debarunsengupta2512@gmail.com>
2018-09-20 16:42:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app=expres();
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+//res.end('<h1>Hello World</h1>');
 });
 app.get('/',function (req,res){
     //res.send('Swarup Bam');
