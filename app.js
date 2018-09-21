051e582df0698209914c5cde87921bd35e546472
Debarun <debarunsengupta2512@gmail.com>
2018-09-21 11:44:32 +0530

    committed nxt day 21 Aug

--- app.js
+++ app.js
@@ -2,20 +2,32 @@
 'use strict';
 const express = require('express');
 const http = require('https');
-
+const jsforce = require('jsforce'); 
 var port = process.env.PORT || 8080;
+var conn = new jsforce.Connection({ 
+    loginUrl: 'https://login.salesforce.com', //'https://login.salesforce.com', 
+    version: '43.0' 
+}); 
+ 
 // create serve and configure it.
 const server = express();
 
 server.post('/',function (request,response)  {
-  
-                    response.setHeader('Content-Type', 'application/json');
+	conn.login(process.env.username, process.env.pass, function(err, res) { 
+    if (err) { 
+        return console.error(err); 
+    }
+     else
+	 {
+	 	 response.setHeader('Content-Type', 'application/json');
                     response.send(JSON.stringify({
                         "fulfillmentText" : "Connected to Salesforce",
                         "source" : "EchoService"
                     })); 
+	 }
                 
-            });
+     });
+});
     
 
 server.get('/',function (req,res){
