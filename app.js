adb291aa875d7b60c2c24e11d969aa5fe35cd97a
Debarun <debarunsengupta2512@gmail.com>
2018-09-23 14:34:58 +0530

    testappjs

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 const express = require('express');
 const http = require('https');
 const jsforce = require('jsforce'); 
+var app=require('./googleActions');
+
+
 var port = process.env.PORT || 8080;
+//var arr = new Array();
 var conn = new jsforce.Connection({ 
     loginUrl: 'https://login.salesforce.com', //'https://login.salesforce.com', 
     version: '43.0' 
@@ -11,33 +15,8 @@ var conn = new jsforce.Connection({
  
 // create serve and configure it.
 const server = express();
+server.post('/fulfillment',app);
 
-server.post('/',function (request,response)  {
-	conn.login(process.env.username, process.env.pass, function(err, res) { 
-    if (err) { 
-	response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "fulfillmentText" : "Error from Salesforce",
-                        "source" : "EchoService"
-                    })); 
-        return console.error(err); 
-    }
-     else
-	 {
-	 	 response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "fulfillmentText" : "Connected to Salesforce",
-                        "source" : "EchoService"
-                    })); 
-	 }
-                
-     });
-});
-    
-
-server.get('/',function (req,res){
-    res.send('Hello World');
-});
 server.listen(port, function () {
     console.log("Server is up and running...");
 });
