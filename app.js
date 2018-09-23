8fbd692cc938d3a15e2495047fdfd727fac4f077
debarunsengupta <debarunsengupta2512@gmail.com>
2018-09-23 15:00:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,42 @@
 'use strict';
 const express = require('express');
 const http = require('https');
+const bodyParser=require('body-parser');
 const jsforce = require('jsforce'); 
-var app=require('./googleActions');
+const server = express();
+
+const {
+    dialogflow,
+    SimpleResponse,
+    Image,
+  } = require('actions-on-google');
 
 
-var port = process.env.PORT || 8080;
+server.use(bodyParser.json());
+// create serve and configure it.
+
+
+var app=dialogflow();
+
+
+app.intent('connect_salesforce',(conv)=>{
+    conv.ask(new SimpleResponse({speech:"We are able to connect to your account",text:"We are able to connect your account"}));
+});
+
+var port = process.env.PORT || 3000;
 //var arr = new Array();
 var conn = new jsforce.Connection({ 
-    loginUrl: 'https://login.salesforce.com', //'https://login.salesforce.com', 
-    version: '43.0' 
+  loginUrl: 'https://login.salesforce.com', //'https://login.salesforce.com', 
+  version: '43.0' 
 }); 
- 
-// create serve and configure it.
-const server = express();
+
+
 server.post('/fulfillment',app);
+
+
+
+
+
 
 server.listen(port, function () {
     console.log("Server is up and running...");
