69a07c7780fd3015de9f5945ec53da9e05d85668
debarunsengupta <debarunsengupta2512@gmail.com>
2018-09-24 22:30:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.intent('connect_salesforce',(conv,params)=>{
 
 app.intent('AccountName',(conv,params)=>{
 	return accountCreation(params.AccountName).then((resp)=>{
-		conv.ask(new SimpleResponse({speech:"We are able to create your account named:"+params.AccountName,text:"We are able to create your account named:"+params.AccountName);
+		conv.ask(new SimpleResponse({speech:"We are able to create your account named:"+params.AccountName,text:"We are able to create your account named:"+params.AccountName}));
 	}).catch((err)=>{
 	conv.ask(new SimpleResponse({speech:"Error while creating salesforce account",text:"Error while creating salesforce account"}));});	
 });
