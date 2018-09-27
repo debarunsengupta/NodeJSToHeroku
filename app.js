7b1bc6f136a47fe99af671cdcc89bc3080991599
debarunsengupta <debarunsengupta2512@gmail.com>
2018-09-27 14:45:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -83,6 +83,8 @@ var accountRetrieval=function (days){
 });
 }
 
+
+
 var convertlead=function (leadname,leadidfetched){
 	return new Promise((resolve,reject)=>{
 		console.log('leadname -->',leadname);
@@ -114,7 +116,7 @@ conn.login(process.env.username, process.env.pass, function(err, res){
 		});
 });
 }
-
+/*
 function leadid(leadname,callback)
 {
 	conn.login(process.env.username, process.env.pass, function(err, res){
@@ -137,8 +139,58 @@ function leadid(leadname,callback)
 		});
 	
 	
+}*/
+
+var leadid=function (leadname){
+	return new Promise((resolve,reject)=>{
+		console.log('leadname -->',leadname);
+		conn.login(process.env.username, process.env.pass, function(err, res){
+			if(err){reject(err);}
+			else{
+				
+			
+			   
+                conn.query('select Id,Name from Lead where Name =\''+leadname+'\'', function(err, result){
+                    if (err) {
+                        reject(err);
+                    }
+                    else{
+                        resolve(result.records[0].Id);
+                    }
+                });
+			
+            }
+		});
+});
 }
 
+var leaddetails=function (leadname){
+	return new Promise((resolve,reject)=>{
+		console.log('leadname -->',leadname);
+		conn.login(process.env.username, process.env.pass, function(err, res){
+			if(err){reject(err);}
+			else{
+			  console.log('Query is:'+'select Id,ConvertedAccount.Name, ConvertedContact.Name, ConvertedOpportunity.Name from Lead where Name =\''+leadname+'\'');
+		          conn.query('select Id,ConvertedAccount.Name, ConvertedContact.Name, ConvertedOpportunity.Name from Lead where Name =\''+leadname+'\'', function(err, result){
+                    if (err) {
+                        console.log('err in fetching lead id:'+err);
+						 reject(err);
+                    }
+                    else{
+			    console.log("result:",result);
+			    //console.log("result record:",typeof(result.records[0]));
+			  //return result.records[0].Id;
+			   resolve(result);
+			    //return callback(result);
+                        
+                    }
+                });
+            }
+		});
+  
+});
+}
+/*
 function leaddetails(leadname,callback)
 {
 	conn.login(process.env.username, process.env.pass, function(err, res){
@@ -161,7 +213,7 @@ function leaddetails(leadname,callback)
 		});
 	
 	
-}
+}*/
 
 app.intent('connect_salesforce',(conv,params)=>{
     
@@ -222,69 +274,35 @@ app.intent('getAccInfo',(conv,params)=>{
 
 app.intent('ConvertLead',(conv,params)=>{
     console.log('lead name:'+params.leadname);
-	var reqleadid;
-	var leadidfet;
-	var str='';
-	var finalresult;
-	 var leadidfetched=leadid(params.leadname,
-				 function(response){
-		 console.log('lead id here:'+response);
-		 reqleadid=response;
-		 leadidfet=response;
-		 
-		 return convertlead(params.leadname,response).then((resp)=>{
-        console.log('response',resp);
+	
+	return leadid(params.leadname).then((resp)=>{
+        console.log('response',resp); //lead id
+       return convertlead(params.leadname,resp).then((resp)=>{
+        console.log('response fetched while calling apex service: ',resp);
 		 console.log('Inside called 3');
-        /*for (var i = 0; i < resp.records.length; i++) {
-            console.log("record name: : " + resp.records[i].Name);
-            console.log("record id: : " + resp.records[i].Id);
-            strName += resp.records[i].Name + ',';
-           
-       }
-		strName=strName.slice(0,-1);*/
-		reqleadid='success';
-		//conv.ask(new SimpleResponse({speech:"Lead Converted Successfully",text:"Lead Converted Successfully"}));
-		
-		 if(reqleadid=='success')
-	     {
-			 console.log('Inside called 1');
-		  str='The Lead converted account name is ';
-		 leaddetails(params.leadname,function(response)
-		 {
-			 console.log('Inside called 2');
-			 console.log('response here:',response);
-			  console.log('response records here:',response.records);
-			  //console.log('response records here length:',response.records.length);
-			
-			console.log('Inside called 6');
-				console.log('response.records[0].ConvertedAccount.Name',response.records[0].ConvertedAccount["Name"]);
-			str+=response.records[0].ConvertedAccount["Name"]+',';
-			str+='The Lead converted contact name is ' +response.records[0].ConvertedContact["Name"]+',';
-			str+='The Lead converted opportunity name is '+response.records[0].ConvertedOpportunity["Name"];
-			 finalresult='success';
-			console.log('str:'+str);
-              });
-		     console.log('str here:'+str);
-			 
-	if(finalresult=='success')
-	 {
-	conv.ask(new SimpleResponse({speech:str,text:str}));
-	 }
-		 
-	    }
-	 else if(reqleadid=='error')
-	 {
-		 conv.ask(new SimpleResponse({speech:"Error while converting Lead",text:"Error while converting Lead"}));
-	 }
-		//conv.ask(new SimpleResponse({speech:str,text:str}));
+      
+		  return leaddetails(params.leadname).then((resp)=>{
+			  var str='The Lead converted account name is ';
+          console.log('Inside called 6');
+		 console.log('response.records[0].ConvertedAccount.Name',resp.records[0].ConvertedAccount["Name"]);
+			str+=resp.records[0].ConvertedAccount["Name"]+',';
+			str+='The Lead converted contact name is ' +resp.records[0].ConvertedContact["Name"]+',';
+			str+='The Lead converted opportunity name is '+resp.records[0].ConvertedOpportunity["Name"];
+		conv.ask(new SimpleResponse({speech:str,text:str}));
+	}).catch((err)=>{
+        console.log('error',err);
+	    conv.ask(new SimpleResponse({speech:"Error while fetching info",text:"Error while fetching info"}));
+		});	
+	
 	}).catch((err)=>{
         console.log('error msg:',err);
 		reqleadid='error';
 	    conv.ask(new SimpleResponse({speech:"Error while converting Lead",text:"Error while converting Lead"}));
 		});
-	 });
-	console.log('str new :'+str);
-	
+		
+	}).catch((err)=>{
+        console.log('error',err);
+	    conv.ask(new SimpleResponse({speech:"Error while fetching lead id",text:"Error while fetching lead id"}));});	
 });
 
 var port = process.env.PORT || 3000;
